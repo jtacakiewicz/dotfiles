2e3ffc7ecb8990c1544ee7539331a44e4a288fb0
Epim3dium <Epim3dium@protonmail.com>
2025-09-12 21:48:31 +0200

    home-manager: flake: updated flake

--- home-manager/.config/home-manager/flake.nix
+++ home-manager/.config/home-manager/flake.nix
@@ -1,25 +1,26 @@
 {
-  description = "Home Manager configuration of jan";
+    description = "Home Manager configuration of jan";
 
-  inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        nix-colors.url = "github:misterio77/nix-colors";
     };
-    nix-colors.url = "github:misterio77/nix-colors";
-  };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."jan" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home.nix ];
-      };
-    };
+    outputs = { nixpkgs, home-manager, ... }@inputs:
+        let
+            system = "x86_64-linux";
+            pkgs = nixpkgs.legacyPackages.${system};
+        in
+            {
+            homeConfigurations."jamjan" = home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                extraSpecialArgs = { inherit inputs; };
+                modules = [ ./home.nix ];
+            };
+        };
 }
+
