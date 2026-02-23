0798082571181fc45334add3b0b52ad486ceec7e
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2026-02-23 19:27:38 +0100

    nix: webapps: corrected webapps to use firefox instead of chrome

--- nix/nix/home-manager/modules/webapps.nix
+++ nix/nix/home-manager/modules/webapps.nix
@@ -1,11 +1,10 @@
 { config, pkgs, ... }:
 
-
 {
-
     home.packages = [
-        pkgs.chromium
+        pkgs.firefox
     ];
+
     home.file.".local/share/icons/discord.svg".source = ./assets/discord.svg;
     home.file.".local/share/icons/nixos.svg".source = ./assets/nixos.svg;
     home.file.".local/share/icons/messenger.svg".source = ./assets/messenger.svg;
@@ -15,31 +14,32 @@
         Version=2.0
         Type=Application
         Name=Discord
-        Exec=${pkgs.chromium}/bin/chromium --app=https://discord.com/channels/@me
+        Exec=${pkgs.firefox}/bin/firefox https://discord.com/channels/@me
         Icon=discord
         Terminal=false
         Categories=Network;
     '';
+
     home.file.".local/share/applications/nixpkgs-web.desktop".text = ''
         [Desktop Entry]
         Version=2.0
         Type=Application
         Name=Nix pkgs
-        Exec=${pkgs.chromium}/bin/chromium --app=https://search.nixos.org/packages
+        Exec=${pkgs.firefox}/bin/firefox https://search.nixos.org/packages
         Icon=nixos
         Terminal=false
         Categories=Network;
     '';
+
     home.file.".local/share/applications/messenger.desktop".text = ''
         [Desktop Entry]
         Version=2.0
         Type=Application
         Name=Messenger
-        Exec=${pkgs.chromium}/bin/chromium --app=https://www.messenger.com
+        Exec=${pkgs.firefox}/bin/firefox https://www.messenger.com
         Icon=messenger
         Terminal=false
         Categories=Network;
     '';
-
 }
 
