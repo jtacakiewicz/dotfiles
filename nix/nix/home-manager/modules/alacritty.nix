24cd97a70c325c6254cc38d96aed49b3eeefb2c2
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2026-05-31 15:57:47 +0200

    nix: alacritty: update to default terminal-exec

--- nix/nix/home-manager/modules/alacritty.nix
+++ nix/nix/home-manager/modules/alacritty.nix
@@ -5,4 +5,11 @@
         pkgs.nerd-fonts.jetbrains-mono
         pkgs.alacritty
     ];
+    home.sessionVariables = {
+        TERMINAL = "alacritty";
+    };
+    xdg.terminal-exec = {
+        enable = true;
+        settings.default = [ "alacritty" ];
+    };
 }
