2a7d2a8c54db26155a5e058063862d01e2bdb78e
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2026-06-22 19:27:40 +0200

    sway: update

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
-
+#!/usr/bin/env bash
 while true; do
-    DATE_TIME=$(date "+%A, %d %B | %H:%M:%S")
+    vol=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+(?=%)' | head -n1 | tr -d '[:space:]')
+    
+    [ -z "$vol" ] && vol=0
+    
+    pos=$(( (vol + 5) / 10 ))
+    [ $pos -gt 10 ] && pos=10
 
-    echo "$DATE_TIME"
+    bar="───────────"
+    slider="${bar:0:pos}○${bar:pos+1}"
+
+    printf "%s %d%% │ %s\n" "$slider" "$vol" "$(date "+%A, %d %B | %H:%M:%S")"
 
     sleep 1
 done
